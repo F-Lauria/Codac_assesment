d7a31575cc2c3edf765f6d46f25442151b887e8f
Francesco <francesco.lauria@nl.abnamro.com>
2023-07-06 18:12:52 +0200

    dummy commit

--- src/tests/test_functions.py
+++ src/tests/test_functions.py
@@ -2,6 +2,7 @@ import argparse
 from src.spark_codac.functions import get_args
 
 
+
 def test_get_arguments_from_argparse(mocker):
     mocker.patch('argparse.ArgumentParser.parse_args',
                  return_value=argparse.Namespace(file_one='one_data.csv', file_two='two_data.csv', countries=['France', 'United States']))
